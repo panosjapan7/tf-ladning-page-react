326df88d0e0eb63468b43b93761ea20239c7c2f2
panosjapan7 <panosjapan@gmail.com>
2022-11-12 10:50:16 +0100

    Creates Masthead

---
+++ src/components/Masthead.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "../styles/styles.css"
+
+const Masthead = () => {
+  return (
+    <header class="masthead">
+            <div class="container">
+                <div class="masthead-subheading">Welcome To Our Studio!</div>
+                <div class="masthead-heading text-uppercase">It's Nice To Meet You</div>
+                <a class="btn btn-primary btn-xl text-uppercase" href="#services">Tell Me More</a>
+            </div>
+        </header>
+  )
+}
+
+export default Masthead
