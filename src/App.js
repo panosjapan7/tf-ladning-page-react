23f8437c213975d51cf9a315b53c54d20aa46747
panosjapan7 <panosjapan@gmail.com>
2022-11-12 10:49:48 +0100

    Renders all components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import './App.css';
 import "./styles/styles.css";
 import Navigation from './components/Navigation';
+import Masthead from './components/Masthead';
+import Services from './components/Services';
+import PortfolioGrid from './components/PortfolioGrid';
+import About from './components/About';
+import Team from './components/Team';
+import Clients from './components/Clients';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="body">
       <Navigation/>
+      <Masthead />
+      <Services />
+      <PortfolioGrid />
+      <About />
+      <Team />
+      <Clients />
+      <Footer />
     </div>
   );
 }
